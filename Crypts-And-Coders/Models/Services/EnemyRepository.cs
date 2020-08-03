dfab0da3d907314034cfd098e5a5223116b09b24
Andrew Smith <andrewcs149@gmail.com>
2020-08-03 16:40:03 -0700

    Add summary comments to EnemiesRepository.

--- Crypts-And-Coders/Models/Services/EnemyRepository.cs
+++ Crypts-And-Coders/Models/Services/EnemyRepository.cs
@@ -17,6 +17,11 @@ namespace Crypts_And_Coders.Models.Services
             _context = context;
         }
 
+        /// <summary>
+        /// Creates a new enemy in the database
+        /// </summary>
+        /// <param name="enemy">enemy information for creation</param>
+        /// <returns>Successful result of enemy creation</returns>
         public async Task<Enemy> Create(Enemy enemy)
         {
             Enemy entity = new Enemy()
@@ -34,6 +39,11 @@ namespace Crypts_And_Coders.Models.Services
             return enemy;
         }
 
+        /// <summary>
+        /// Delete a enemy from the database
+        /// </summary>
+        /// <param name="id">Id of enemy to be deleted</param>
+        /// <returns>Task of completion for enemy delete</returns>
         public async Task Delete(int id)
         {
             Enemy enemy = await GetEnemy(id);
@@ -41,18 +51,33 @@ namespace Crypts_And_Coders.Models.Services
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Get a list of all enemies in the database
+        /// </summary>
+        /// <returns>Successful result with list of enemies</returns>
         public async Task<List<Enemy>> GetEnemies()
         {
             var enemy = await _context.Enemy.ToListAsync();
             return enemy;
         }
 
+        /// <summary>
+        /// Get a specific enemy in the database by ID
+        /// </summary>
+        /// <param name="id">Id of enemy to search for</param>
+        /// <returns>Successful result of specified enemy</returns>
         public async Task<Enemy> GetEnemy(int id)
         {
             Enemy enemy = await _context.Enemy.FindAsync(id);
             return enemy;
         }
 
+        /// <summary>
+        /// Update a given enemy in the database
+        /// </summary>
+        /// <param name="id">Id of enemy to be updated</param>
+        /// <param name="enemy">Enemy information for update</param>
+        /// <returns>Successful result of specified updated enemy</returns>
         public async Task<Enemy> Update(Enemy enemy)
         {
             _context.Entry(enemy).State = EntityState.Modified;
