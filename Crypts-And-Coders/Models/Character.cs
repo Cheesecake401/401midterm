e3b6f63e4618baad9c4ba1c35c86dea8cc6e2182
Nicco Ryan <niccoryan0@gmail.com>
2020-08-02 17:21:19 -0700

    Scaffolding files

---
+++ Crypts-And-Coders/Models/Character.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crypts_And_Coders.Models
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public Species Species { get; set; }
+        public Class Class { get; set;}
+    }
+
+    public enum Species
+    {
+        Human,
+        Elf,
+        Gnome,
+        Dwarf,
+        HalfOrc,
+        Dragonborn
+    }
+
+    public enum Class
+    {
+        Paladin,
+        Wizard,
+        Ranger,
+        Thief,
+        Bard,
+        Cleric,
+        Monk,
+        Fighter,
+        Druid
+    }
+}
