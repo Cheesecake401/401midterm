5fbd8490e44b2946a231e58e13ac95182cd9cbbd
Andrew Smith <andrewcs149@gmail.com>
2020-08-03 14:45:36 -0700

    Add summary comments for IEnemy.

--- Crypts-And-Coders/Models/Interfaces/IEnemy.cs
+++ Crypts-And-Coders/Models/Interfaces/IEnemy.cs
@@ -7,19 +7,39 @@ namespace Crypts_And_Coders.Models.Interfaces
 {
     public interface IEnemy
     {
-        //Create a enemy
+        /// <summary>
+        /// Creates a new enemy in the database
+        /// </summary>
+        /// <param name="enemy">Enemy information for creation</param>
+        /// <returns>Successful result of enemy creation</returns>
         Task<Enemy> Create(Enemy enemy);
 
-        //Read a enemy/Get
+        /// <summary>
+        /// Get a list of all enemies in the database
+        /// </summary>
+        /// <returns>Successful result with list of enemies</returns>
         Task<List<Enemy>> GetEnemies();
 
-        //Get by enemy id
-        Task<Enemy> GetEnemies(int id);
+        /// <summary>
+        /// Get a specific enemy in the database by ID
+        /// </summary>
+        /// <param name="id">Id of enemy to search for</param>
+        /// <returns>Successful result of specified enemy</returns>
+        Task<Enemy> GetEnemy(int id);
 
-        //Update a enemy
+        /// <summary>
+        /// Update a given enemy in the database
+        /// </summary>
+        /// <param name="id">Id of enemy to be updated</param>
+        /// <param name="enemy">Enemy information for update</param>
+        /// <returns>Successful result of specified updated enemy</returns>
         Task<Enemy> Update(int id, Enemy enemy);
 
-        //Delete a enemy
+        /// <summary>
+        /// Delete a enemy from the database
+        /// </summary>
+        /// <param name="id">Id of enemy to be deleted</param>
+        /// <returns>Task of completion for enemy delete</returns>
         Task Delete(int id);
     }
 }
