4d5e144242c5ff5bea691401539b5ce56d090576
Olamide Ogunde <oogunde0413@gmail.com>
2020-08-03 11:13:23 -0700

    added repositorys

---
+++ Crypts-And-Coders/Models/Services/EnemyRepository.cs
@@ -0,0 +1,38 @@
+﻿using Crypts_And_Coders.Data;
+using Crypts_And_Coders.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crypts_And_Coders.Models.Services
+{
+    public class EnemyRepository : IEnemy
+    {
+    private CryptsDbContext _context;
+        public Task<Enemy> Create(Enemy enemy)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Enemy>> GetEnemies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Enemy> GetEnemies(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Enemy enemy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Crypts-And-Coders/Models/Services/LocationsRepository.cs
@@ -0,0 +1,36 @@
+﻿using Crypts_And_Coders.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crypts_And_Coders.Models.Services
+{
+    public class LocationsRepository : ILocation
+    {
+        public Task<Location> Create(Location location)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Location>> GetLocations()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Location> GetLocations(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Location location)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
