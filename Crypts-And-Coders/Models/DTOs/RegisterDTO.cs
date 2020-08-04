4291f0696332e4a9cff2f22994c637d51d592fab
Nicco Ryan <niccoryan0@gmail.com>
2020-08-04 13:16:38 -0700

    Working on authorization

---
+++ Crypts-And-Coders/Models/DTOs/RegisterDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crypts_And_Coders.Models.DTOs
+{
+    public class RegisterDTO
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
