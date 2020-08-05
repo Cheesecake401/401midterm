627c8db7be9b1932b5751c226f91fd8f89e8f703
Andrew Smith <andrewcs149@gmail.com>
2020-08-05 15:47:57 -0700

    Added viewModels folder.

---
+++ Crypts-And-Coders/ViewModels/CharacterAndSpeciesClasses.cs
@@ -0,0 +1,19 @@
+﻿using Crypts_And_Coders.Models;
+using Crypts_And_Coders.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Crypts_And_Coders.Models.SpeciesAndClass;
+
+namespace Crypts_And_Coders.ViewModels
+{
+    public class CharacterAndSpeciesClasses
+    {
+        public CharacterDTO CharacterDTO { get; set; }
+
+        //public Species Species { get; set; }
+        //public Class Class { get; set; }
+        public SpeciesAndClass SpeciesAndClass { get; set; }
+    }
+}
